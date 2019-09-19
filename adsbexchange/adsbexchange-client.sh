4b970d40a22dbb6b204ef38a8dfa7a71f57659e0
Jonathan Bradshaw <jonathan@JBMacBook.home>
2019-09-19 16:06:19 -0400

    initial

---
+++ adsbexchange/adsbexchange-client.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -o errexit          # Exit on most errors (see the manual)
+#set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+#set -o pipefail         # Use last non-zero exit code in a pipeline
+set -o xtrace          # Trace the execution of the script (debug)
+
+DUMP1090_SERVER='dump1090'
+DUMP1090_PORT='30005'
+
+echo "Read ADSBEXCHANGE_PORT=${ADSBEXCHANGE_PORT}"
+
+while true; do
+  echo "Starting replay from TCP:${DUMP1090_SERVER}:${DUMP1090_PORT} to TCP:feed.adsbexchange.com:${ADSBEXCHANGE_PORT}"
+
+  set +o errexit
+  socat -u "TCP:${DUMP1090_SERVER}:${DUMP1090_PORT}" "TCP:feed.adsbexchange.com:${ADSBEXCHANGE_PORT}"
+  SOCAT_STATUS=${?}
+  set -o errexit
+
+  echo "Replay ended"
+
+  if [[ "${SOCAT_STATUS}" -eq 0 ]]; then
+    echo "Replay ended without failure"
+    break
+  else
+    echo "Replay ended with failure (${SOCAT_STATUS}) - restarting"
+  fi
+done
+
+exit ${SOCAT_STATUS}

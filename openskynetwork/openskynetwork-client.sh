82058375fbf32a6e005328f58214dcbe407de0dd
Jonathan Bradshaw <jonathan@JBMacBook.home>
2019-09-19 16:15:50 -0400

    initial

---
+++ openskynetwork/openskynetwork-client.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+echo "opensky-feeder openskyd/latitude string ${OPENSKY_LATITUDE}" | debconf-set-selections
+echo "opensky-feeder openskyd/longitude string ${OPENSKY_LONGITUDE}" | debconf-set-selections
+echo "opensky-feeder openskyd/altitude string ${OPENSKY_ALTITUDE}" | debconf-set-selections
+echo "opensky-feeder openskyd/username string ${OPENSKY_USER}" | debconf-set-selections
+echo "opensky-feeder openskyd/serial string ${OPENSKY_SERIAL}" | debconf-set-selections
+echo "opensky-feeder openskyd/host string ${DUMP1090_HOST}" | debconf-set-selections
+echo "opensky-feeder openskyd/port string ${DUMP1090_PORT}" | debconf-set-selections
+echo "opensky-feeder openskyd/dump1090branch string default" | debconf-set-selections
+dpkg-reconfigure -u opensky-feeder
+
+exec /usr/bin/openskyd-dump1090

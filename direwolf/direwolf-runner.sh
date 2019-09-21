b961447578e8d6d4b090293e3d07651e343b58ab
Jonathan Bradshaw <jonathan@Jonathans-iMac.local>
2019-09-21 15:39:09 -0400

    Bugfix

--- direwolf/direwolf-runner.sh
+++ direwolf/direwolf-runner.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-cd /root
 
 # Update configuration using environment variables
 if [ -n "$MYCALL" ]; then
-  sed -i 's|^MYCALL.*$|MYCALL '"$MYCALL"'|g' sdr.conf
+  sed -i 's|^MYCALL.*$|MYCALL '"$MYCALL"'|g' /etc/sdr.conf
 fi
 
 if [ -n "$IGSERVER" ]; then
-  sed -i 's|^IGSERVER.*$|IGSERVER '"$IGSERVER"'|g' sdr.conf
+  sed -i 's|^IGSERVER.*$|IGSERVER '"$IGSERVER"'|g' /etc/sdr.conf
 fi
 
 if [ -n "$IGLOGIN" ]; then
-  sed -i 's|^IGLOGIN.*$|IGLOGIN '"$IGLOGIN"'|g' sdr.conf
+  sed -i 's|^IGLOGIN.*$|IGLOGIN '"$IGLOGIN"'|g' /etc/sdr.conf
 fi
 
 # Start Direwolf
-rtl_fm -f $FREQUENCY ${*} - | direwolf -c sdr.conf -t 0 -r 24000 -D 1 -t 0 -a 60 -
+rtl_fm -f ${FREQUENCY} -d ${DEVICE} ${*} - | direwolf -c /etc/sdr.conf -t 0 -r 24000 -D 1 -t 0 -a 60 -

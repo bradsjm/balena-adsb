3ef37950108cbfd26d31d504858bb4e7f2c54e06
Jonathan Bradshaw <jonathan@Jonathans-iMac.local>
2019-09-21 15:35:55 -0400

    Bugfix setkey

--- radarbox24/radarbox24-client.sh
+++ radarbox24/radarbox24-client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-set -x
+#set -x
 exec /usr/bin/rbfeeder \
-  --set-key ${RADARBOX24_KEY} \
+  --setkey ${RADARBOX24_KEY} \
   --set-network-mode on \
   --set-network-host $(getent hosts ${DUMP1090_HOST} | head -n 1 | awk '{print $1}') \
   --set-network-port ${DUMP1090_PORT} \

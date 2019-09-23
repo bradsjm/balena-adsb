d338f1c591592aabfaf22e0ab5f37c09d6c45c22
Jonathan Bradshaw <jonathan@JBMacBook.home>
2019-09-23 12:57:32 -0400

    updates

---
+++ dump1090/dump1090-runner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -o errexit          # Exit on most errors (see the manual)
+#set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+#set -o pipefail         # Use last non-zero exit code in a pipeline
+#set -o xtrace          # Trace the execution of the script (debug)
+
+exec nice -n -5 dump1090 --device-index ${DEVICE} $RECEIVER_OPTIONS $DECODER_OPTIONS $NET_OPTIONS

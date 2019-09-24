8cb0a95610e5fb1b92f453a358d6329dbce331f0
Jonathan Bradshaw <jonathan@JBMacBook.home>
2019-09-23 20:05:30 -0400

    fixes

--- modesmixer2/modesmixer2-runner.sh
+++ modesmixer2/modesmixer2-runner.sh
@@ -6,7 +6,7 @@ set -o nounset          # Disallow expansion of unset variables
 #set -o pipefail         # Use last non-zero exit code in a pipeline
 #set -o xtrace          # Trace the execution of the script (debug)
 
-modesmixer2 --web 8080 \
+exec modesmixer2 --web 8080 \
             --inConnectId "${DUMP1090_SERVER}:${DUMP1090_PORT}:DUMP1090" \
             --inConnectId "flightaware:30105:MLAT" \
             --location "${LATITUDE}:${LONGITUDE}" \
